9dbd403a8eba690c2bc99ceb93ad93b6a2c686d8
tlgms <siheun9@gmail.com>
2026-02-13 10:17:59 +0900

    feat: Feign 인터페이스 빈 등록(#4)

--- src/main/java/demago/khjv2/KhJv2Application.java
+++ src/main/java/demago/khjv2/KhJv2Application.java
@@ -2,8 +2,10 @@ package demago.khjv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class KhJv2Application {
 
     public static void main(String[] args) {
