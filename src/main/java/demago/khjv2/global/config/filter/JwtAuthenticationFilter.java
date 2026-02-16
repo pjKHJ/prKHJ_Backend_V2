927eee5d5d4464cd3f676c4f183760682ca939a2
tlgms <siheun9@gmail.com>
2026-02-17 01:39:15 +0900

    feat: JwtAuthenticationFilter.java 추가(#7)

---
+++ src/main/java/demago/khjv2/global/config/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package demago.khjv2.global.config.filter;
+
+import demago.khjv2.global.security.jwt.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider tokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String bearer = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearer) && bearer.startsWith("Bearer ")) {
+            String token = bearer.substring(7);
+            if (tokenProvider.validateToken(token)) {
+                SecurityContextHolder.getContext().setAuthentication(tokenProvider.getAuthentication(token));
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
