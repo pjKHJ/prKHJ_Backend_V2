750fac01c48f49fb0b038f0435ad246064b31530
tlgms <siheun9@gmail.com>
2026-02-17 01:32:57 +0900

    feat: UserDetails 구현 -> CustomUserDetails.java 추가(#7)

---
+++ src/main/java/demago/khjv2/global/security/userdetails/CustomUserDetails.java
@@ -0,0 +1,72 @@
+package demago.khjv2.global.security.userdetails;
+
+import demago.khjv2.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final Long id;
+    private final String username;
+    private final String password;
+    private final List<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(Long id, String username, String password,
+                             List<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static CustomUserDetails from(User user) {
+        return new CustomUserDetails(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 이메일 인증을 구현하지 않으니 일단 true
+        return true;
+    }
+}
