baa989fedc6725afe6fcf83910f3ccb0ef1af5ce
tlgms <siheun9@gmail.com>
2026-02-17 01:50:36 +0900

    fix: user는 SQL 예약어로 테이블 생성/쿼리 시 문제가 발생할 수 있기 때문에 테이블명을 uses로 변경(#7)

--- src/main/java/demago/khjv2/domain/user/entity/User.java
+++ src/main/java/demago/khjv2/domain/user/entity/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
