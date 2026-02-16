5c72af4d36c96438e736ea6effeaf1486dce2ab2
tlgms <siheun9@gmail.com>
2026-02-17 02:26:17 +0900

    feat: 인증 코드 확인 서비스 추가(#7)

---
+++ src/main/java/demago/khjv2/domain/user/service/VerificationService.java
@@ -0,0 +1,18 @@
+package demago.khjv2.domain.user.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VerificationService {
+
+    private final String expectedCode;
+
+    public VerificationService(@Value("${spring.user.code}") String expectedCode) {
+        this.expectedCode = expectedCode;
+    }
+
+    public boolean verify(String inputCode) {
+        return expectedCode.equals(inputCode);
+    }
+}
