520c4727795d0f5757f26987d234b6b616649330
tlgms <siheun9@gmail.com>
2026-03-11 19:30:04 +0900

    feat: 이전에 구현을 안했던 리스트 요청과 테스트 추가(#13)

--- src/main/java/demago/khjv2/domain/student/presentation/ManagementController.java
+++ src/main/java/demago/khjv2/domain/student/presentation/ManagementController.java
@@ -3,19 +3,23 @@ package demago.khjv2.domain.student.presentation;
 import demago.khjv2.domain.student.presentation.dto.request.StudentBulkCreateRequest;
 import demago.khjv2.domain.student.presentation.dto.request.StudentCreateRequest;
 import demago.khjv2.domain.student.presentation.dto.request.StudentDeleteRequest;
+import demago.khjv2.domain.student.presentation.dto.response.ManagementStudentListResponse;
 import demago.khjv2.domain.student.presentation.dto.response.StudentBulkCreateResponse;
 import demago.khjv2.domain.student.presentation.dto.response.StudentDeleteResponse;
 import demago.khjv2.domain.student.presentation.dto.response.StudentResponse;
+import demago.khjv2.domain.student.service.StudentQueryService;
 import demago.khjv2.domain.student.service.StudentService;
+import demago.khjv2.domain.studentDetail.service.StudentDetailsSyncService;
+import demago.khjv2.domain.studentGrass.entity.BojGrassHistory;
+import demago.khjv2.domain.studentGrass.repository.BojGrassHistoryRepository;
+import demago.khjv2.domain.studentGrass.service.StudentGrassSyncService;
+import demago.khjv2.global.scheduler.BojDetailsScheduler;
+import demago.khjv2.global.scheduler.BojGrassScheduler;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v2/management/students")
@@ -23,16 +27,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ManagementController {
 
     private final StudentService studentService;
+    private final StudentQueryService studentQueryService;
 
+    private final StudentGrassSyncService studentGrassSyncService;
+    private final StudentDetailsSyncService studentDetailsSyncService;
 
-    //단건 학생 등록
+    @GetMapping
+    public ResponseEntity<ManagementStudentListResponse> getStudents() {
+        return ResponseEntity.ok(studentQueryService.getManagementStudents());
+    }
+
     @PostMapping
     public ResponseEntity<StudentResponse> createStudent(@Valid @RequestBody final StudentCreateRequest request) {
         return ResponseEntity.ok(studentService.createStudent(request));
     }
 
-
-    //일괄 학생 등록
     @PostMapping("/bulk")
     public ResponseEntity<StudentBulkCreateResponse> createBulkStudents(
             @Valid @RequestBody final StudentBulkCreateRequest request
@@ -40,9 +49,19 @@ public class ManagementController {
         return ResponseEntity.ok(studentService.createBulkStudents(request));
     }
 
-    //학생 삭제(ids 기반).
     @DeleteMapping
     public ResponseEntity<StudentDeleteResponse> deleteStudents(@Valid @RequestBody final StudentDeleteRequest request) {
         return ResponseEntity.ok(studentService.deleteStudents(request));
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        studentDetailsSyncService.syncUserDetail();
+
+        studentGrassSyncService.syncGrass();
+
+        studentDetailsSyncService.flame();
+
+        return;
     }
 }
