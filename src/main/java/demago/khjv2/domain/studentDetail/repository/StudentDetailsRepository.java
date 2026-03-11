12f68ff009760d22619d7ecabd001ae9b37d3061
tlgms <siheun9@gmail.com>
2026-03-11 20:23:28 +0900

    feat: JpaRepository의 ID 제네릭 타입이 엔티티 PK 타입과 맞지 않던 문제 수정(#13)

--- src/main/java/demago/khjv2/domain/studentDetail/repository/StudentDetailsRepository.java
+++ src/main/java/demago/khjv2/domain/studentDetail/repository/StudentDetailsRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface StudentDetailsRepository extends JpaRepository<StudentDetails,Integer> {
+public interface StudentDetailsRepository extends JpaRepository<StudentDetails,Long> {
     Optional<StudentDetails> findByStudentId(Student studentId);
 }
